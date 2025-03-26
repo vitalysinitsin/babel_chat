c5be883d176b3e5dd4708330531c6701c43549d9
hungryvito <vsinitsin94@gmail.com>
2025-03-25 22:00:59 -0600

    simple registration form setup with bootstrap + react + sass

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,38 @@
-import "./App.css";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import "./App.scss";
 
 function App() {
   return (
-    <div>
-      <h1>Babel Chat</h1>
-    </div>
+    <Container className="pt-5">
+      <Row className="bg-white py-5 justify-content-center">
+        <Col sm={8} md={6} lg={4}>
+          <h1 className="text-center">Register</h1>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Username</Form.Label>
+              <Form.Control type="text" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" />
+            </Form.Group>
+            <div className="text-center">
+              <Button variant="success" type="submit">
+                Register
+              </Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
